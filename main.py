f8478cc83200a6fd880b5f6dd264e2d87e41bde6
Jason Li <jasonli.0616@outlook.com>
2021-03-31 12:08:19 -0400

    Added comments + modified code so easier to fork

--- main.py
+++ main.py
@@ -21,8 +21,6 @@ async def on_ready():
     eventTime = str(dt.now())
     print("Bot is online " + eventTime)
 
-    await client.change_presence(activity=discord.Game(name="p! help"))
-
 banned_imports = [
     "pygame",
     "tkinter",
@@ -30,6 +28,8 @@ banned_imports = [
     "pygui",
     "pysimplegui",
     "kivy",
+    "rm",
+    "rmdir",
 ]
 
 in_banned = False
@@ -38,44 +38,41 @@ in_banned = False
 async def on_message(message):
     global in_banned
 
+    # checks if anything in banned_imports list is in message
     for imports in banned_imports:
         if imports in str(message.content).lower():
             in_banned = True
         else:
             pass
         
+    #if in correct channel
     if str(message.channel) == "pyshell" or str(message.channel) == "bot-code":
+        #if message does not contain anything in banned_imports list and if message is not sent by itself
         if in_banned == False and str(message.author.id) != '825106616826331167':
             inStr = str(message.content)
+            #writes message sent to a .py file
             f = open("files/pyin.py", "w")
             f.write(inStr)
             f.close()
+            #runs that .py file in terminal and writes the terminal output into .txt file
             with open("files/pyout.txt", "w") as output:
                 subprocess.call(["python", "files/pyin.py"], stdout=output)
             output = open("files/pyout.txt", "r")
+            #reads terminal output .txt file  and sends it into same channel as original message
             outStr = str(output.read())
             if outStr != "":
                 await message.channel.send(str(outStr))
             else:
                 pass
 
-            confirmStr = (str(message.author) + " ran:") + "\n" + inStr + "\n" + "out:" + "\n" + outStr
-            confirmChannel = client.get_channel(825545951003148339)
-            print(confirmStr)
-            await confirmChannel.send(confirmStr)
-
-            f = open("files/pyin.py", "w")
-            f.write("")
-            f.close()
-
+        #if message DOES contain anything in banned_imports list and if message is not sent by itself
         elif in_banned == True and str(message.author.id) != '825106616826331167':
             inStr = str(message.content)
+            #sends unavailable message to same channel as original message
             await message.channel.send("Unavailable command")
             in_banned = False
-            print(str(message.author) + " ran:")
-            print(inStr)
-            print("out: Unavailable command")
-            
+        
+        #if message is sent by itself, does nothing
         elif str(message.author.id) == '825106616826331167':
             pass
     else:
@@ -88,17 +85,6 @@ async def test(ctx):
     eventTime = str(dt.now())
     embedTitle = "Bot is online " + eventTime + "UTC"
     embed = discord.Embed(title=embedTitle)
-    await ctx.send(embed=embed)
-
-@client.command()
-async def help(ctx, args=None):
-    embed=discord.Embed(title="PyShell Bot", description="by: jasonli0616", url="https://pyshell-bot.jasonli0616.repl.co", color=0x00ACEE)
-    embed.set_thumbnail(url="https://raw.githubusercontent.com/jasonli0616/PyShell-Discord-Bot/main/pyshell_pfp.png")
-    embed.add_field(name="Python", value="Type python code in pyshell or bot-code channel\nChannel named \"pyshell\" or \"bot-code\" required", inline=False)
-    embed.add_field(name="Warning", value="Importing GUI libraries will sometimes break the bot, please refrain from doing so.", inline=False)
-    embed.add_field(name="Website", value="Visit our website for more info: https://pyshell-bot.jasonli0616.repl.co", inline=False)
-    embed.add_field(name="Source code", value="See source code on GitHub: https://github.com/jasonli0616/PyShell-Discord-Bot", inline=False)
-    embed.add_field(name="Help/Report bug", value="For support or to report a bug, head over to our support server https://discord.gg/mjBrQsu6tw", inline=False)
     await ctx.send(embed=embed)
 
 
